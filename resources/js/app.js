07ba588115282eaa839cb4fca7efbfae5053615f
Alisher <myemail9445@inbox.ru>
2025-07-17 14:05:32 +0500

    vue installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import { Notifications } from '@kyvg/vue3-notification';
+
+
+const app = createApp({
+    data() {
+        return {
+            message: 'Tadbirlar platformasi'
+        };
+    }
+});
+
+
+app.use(Notifications);
+
+const appElement = document.getElementById('app');
+if (appElement) {
+    app.mount("#app");
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
