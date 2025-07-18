d033872f5c82ffc3c3a0e57a0f4cd6bfd1785d6e
Alisher <myemail9445@inbox.ru>
2025-07-18 09:30:43 +0500

    repository service provider

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use App\Application\RepositoryInterfaces\IUserRepository;
+use App\Infrastructure\Repositories\UserRepository;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(IUserRepository::class, UserRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    Mews\Captcha\CaptchaServiceProvider::class
+    App\Providers\RepositoryServiceProvider::class,
+    Mews\Captcha\CaptchaServiceProvider::class,
 ];
