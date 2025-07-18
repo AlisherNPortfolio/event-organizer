8f439df1b2bc88683cfd96e02f36accfb810f59f
Alisher <myemail9445@inbox.ru>
2025-07-18 10:16:06 +0500

    get_exception_message function added

--- app/Utils/helpers.php
+++ app/Utils/helpers.php
@@ -4,3 +4,10 @@ if (!function_exists('is_development')) {
         return app()->environment('local') || app()->environment('development');
     }
 }
+
+if (!function_exists('get_exception_message')) {
+    function get_exception_message(string $message, Exception $exception): string
+    {
+        return is_development() ? $message . ' Error:' . $exception->getMessage() : $message;
+    }
+}
