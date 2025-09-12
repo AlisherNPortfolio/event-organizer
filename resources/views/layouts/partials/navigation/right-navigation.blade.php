c09b09e30b8bebb8a5549ad7241edde8b9aeb729
AlisherN <myemail9445@inbox.ru>
2025-09-12 11:16:55 +0500

    home page route

--- resources/views/layouts/partials/navigation/right-navigation.blade.php
+++ resources/views/layouts/partials/navigation/right-navigation.blade.php
@@ -52,7 +52,7 @@
             </div>
         </div>
     @else
-        <a href="events"
+        <a href="{{ route('events.index') }}"
             class="text-gray-700 hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium">
             Tadbirlar
         </a>
