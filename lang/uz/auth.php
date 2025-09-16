4a8bd8c6fd80748790b0037bf617f6bb7b5f825f
AlisherN <myemail9445@inbox.ru>
2025-09-16 10:21:40 +0500

    localization

---
+++ lang/uz/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Ma\'lumotlar noto\'g\'ri.',
+    'password' => 'Parol noto\'g\'ri.',
+    'throttle' => 'Login qilish uchun juda ko\'p urinildi. Iltimos, :seconds soniyadan so\'ng qayta urinib ko\'ring.',
+
+];
